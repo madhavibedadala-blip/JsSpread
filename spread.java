71387fa134afa229fc88c7b09468dc09a5199a67
Madhavi Bedadala <madhavibedadala@gmail.com>
2026-07-02 14:38:54 +0530

    Add integer product calculation in spread.java
    Implement a program to calculate the product of integers from input.

---
+++ spread.java
@@ -0,0 +1,35 @@
+"use strict";
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+let inputString = "";
+let currentLine = 0;
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((str) => str.trim());
+  main();
+});
+function readLine() {
+  return inputString[currentLine++];
+}
+function getProductOfTheIntegers(...args) {
+  let productOfAllValues = 1;
+  for (let value of args) {
+    productOfAllValues = productOfAllValues * value;
+  }
+  return productOfAllValues;
+}
+function main() {
+  let integersArray = JSON.parse(readLine().replace(/'/g, '"'));
+   let k=1;
+   let n=integersArray.length;
+   for(let i=0;i<n;i++){
+       k*=integersArray[i]
+   }
+   console.log(k)
+}
+
